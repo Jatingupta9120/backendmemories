04ae0907ae5d2c48dcd5a4f76afbda471ff812cc
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2022-04-30 16:43:51 +0530

    All set to Work!

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-// app.get('/', (req, res) => {
-//         res.send('Welcome to Memories API: Created by Aman Singh Bhogal');
-// });
+app.get('/', (req, res) => {
+        res.send('Welcome to Memories API: Created by Aman Singh Bhogal');
+});
 
 // Connecting to MongoDB
 // Details of Cluster:
