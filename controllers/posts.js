feedad48f380516ed5e7605971bb53f727eb6559
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2022-05-21 13:34:06 +0530

    Pagination and Search Functionality added to BackEnd.

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,9 +2,33 @@ import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
+
+    const { page, limit } = req.query;
+
     try {
-        const postMessages = await PostMessage.find();
-        res.status(200).json(postMessages);
+        const defaultPageLimit = 3;
+        const Limit = (Number(limit)? Number(limit): defaultPageLimit);
+        const Page = (Number(page)>0? Number(page) : 1);
+        const StartIndex = (Page - 1) * Limit;
+        const TotalPosts = await PostMessage.countDocuments({});
+
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(Limit).skip(StartIndex);
+
+        res.status(200).json({ data: posts, currentPage: Page, numberOfPages: Math.ceil(TotalPosts/Limit) });
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
+export const getPostsBySearch = async (req, res) => {
+
+    const { searchQuery, tags } = req.query;
+
+    try {
+        const title = new RegExp(searchQuery, 'i');
+        const posts = await PostMessage.find({ $or: [ {title}, {tags: { $in: tags.split(',') }} ] });
+        res.status(200).json({ data: posts });
+
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
